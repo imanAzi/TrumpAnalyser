0bce5b27b6894e1328f07d9e1461b73e6d4a949b
fvantendeloo <36112138+fvantendeloo@users.noreply.github.com>
2018-04-08 13:41:55 +0200

    Alstublieft

---
+++ src/main/java/be/kdg/StemTest.java
@@ -0,0 +1,20 @@
+package be.kdg;
+
+/**
+ * @author Floris Van Tendeloo
+ */
+public class StemTest {
+    public static void main(String[] args) {
+        Stemmer stemmer = new Stemmer();
+        String[] strings = {"cats", "allies", "congratulations", "says"};
+
+        for (String string : strings) {
+            String stemmed = stemmer.stem(string);
+            System.out.println(stemmed);
+        }
+        
+        // allies -> alli ???
+        // says -> sai ????
+        // congratul -> congratulations
+    }
+}
